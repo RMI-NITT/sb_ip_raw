2be24a34def5ee49e2c729e0197412690b084616
Naveen50525 <37217362+Naveen50525@users.noreply.github.com>
2019-03-09 21:03:42 +0530

    Update ip_raw.py

--- ip_raw.py
+++ ip_raw.py
@@ -1,6 +1,11 @@
 import cv2
 import numpy as np
 
+## Top left - 84,4
+## Top right - 594,9
+## Bottom left - 73,479
+## Bottom right - 626,462
+
 video = cv2.VideoCapture(0)
 points_1 = np.float32([(84,4),(594,9),(73,479),(626,462)]) 
 points_2 = np.float32([(0,0),(640,0),(0,480),(640,480)])
@@ -18,20 +23,3 @@ while True:
 
 cv2.destroyAllWindows()    #close all the windows
 video.release()
-
-## Top left - 84,4
-## Top right - 594,9
-## Bottom left - 73,479
-## Bottom right - 626,462
-
-'''
-
-img = cv2.imread('image.jpg')
-rows,cols,ch = img.shape
-M = cv2.getPerspectiveTransform(points_1,points_2)
-dst = cv2.warpPerspective(img,M,(1000,1000))
-plt.subplot(121),plt.imshow(img),plt.title('Input')
-plt.subplot(122),plt.imshow(dst),plt.title('Output')
-plt.show()
-
-'''
